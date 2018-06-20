cfc138f0acee40f2ce71f87eab89cbb7d5e0f6ed
gongxianglin <gxl1053042633@163.com>
2018-06-20 12:39:13 +0800

    add train.py

---
+++ read_data.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+
+
+
+def read_data():
+    # 数据目录
+    data_dir = '~/PycharmProjects/DCGAN/data/minist'
+
+    fd = open(os.path.join(data_dir, 'train_image_idx3_ubyte'))
+    # 转换成numpy数组
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    trX = loaded[16:].reshape((60000)).astype(np.float)
+
+    # 训练label
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    trY = loaded[8:].reshape((60000)).astype(np.float)
+
+    # 测试数据
+    fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    teX = loaded[16:].reshape((10000)).astype(np.float)
+    # 测试label
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    teY = loaded[8:].reshape((10000)).astype(np.float)
+
+    trY = np.asarray(trY)
+    teY = np.asarray(teY)
+
+    X = np.concatenate((trX, teX), axis=0)
+    y = np.concatenate((trY, teY), axis=0)
+
+    # 打乱排序
+    seed = 547
+    np.random.seed(seed)
+    np.random.shuffle(X)
+    np.random.seed(seed)
+    np.random.shuffle(y)
+
+    # 这里y_vec 表示对网络所加对约束条件，这个条件是类别标签
+    # 可以看到，y_vec世纪就是对y对独热编码
+    y_vec = np.zeros((len(y), 10), dtype=np.float)
+    for i, label in enumerate(y):
+        y_vec[i, y[i]] = 1.0
+
+    return X / 255., y_vec
--- train.py
+++ train.py
@@ -20,3 +20,16 @@ def train():
 
     #由生成器生成图像G
 
+    G = generator(z, y)
+    #真实图像送入判别器
+    D, D_logits = discriminator(images, y)
+
+    samples = sampler(z,y)
+    D_, D_logits_ = discriminator(G, y, reuse=True)
+
+    d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits, tf.ones_like(D)))
+    d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.zeros_like(D_)))
+    d_loss = d_loss_real + d_loss_fake
+    g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.ones_like(D_)))
+
+    z_sum = tf.
