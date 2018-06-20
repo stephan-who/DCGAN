d80313b2fb3369ab21ab967ec5a53869d69f37a5
gongxianglin <gxl1053042633@163.com>
2018-06-21 00:55:13 +0800

    add train.py

--- train.py
+++ train.py
@@ -32,4 +32,31 @@ def train():
     d_loss = d_loss_real + d_loss_fake
     g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.ones_like(D_)))
 
-    z_sum = tf.
+    z_sum = tf.summary.histogram("z",z)
+    d_sum = tf.summary.histogram("d",D)
+    d__sum = tf.summary.histogram("d_", D_)
+    G_sum = tf.summary.image("G",G)
+
+    d_loss_real_sum = tf.summary.scalar("d_loss_real", d_loss_real)
+    d_loss_fake_sum = tf.summary.scalar("d_loss_fake", d_loss_fake)
+    d_loss_sum = tf.summary.scalar("d_loss", d_loss)
+    g_loss_sum = tf.summary.scalar("g_loss", g_loss)
+
+    #合并各自的总结
+    g_sum = tf.summary.merge([z_sum, d__sum, G_sum, d_loss_fake_sum, g_loss_sum])
+    d_sum = tf.summary.merge([z_sum, d_sum, d_loss_real_sum, d_loss_sum])
+
+    #生成器和判别器要更新的变量，由于tf.train.Optimizer的var_list
+    t_vars = tf.trainable_variables()
+    d_vars = [var for var in t_vars if 'd_' in var.name]
+    g_vars = [var for var in t_vars if 'g_' in var.name]
+
+    saver = tf.train.Saver()
+
+    #优化算法采用Adam
+    d_optim = tf.train.AdamOptimizer(0.0002, beta1= 0.5).minimize(d_loss, var_list=d_vars, global_step=global_step)
+    g_optim = tf.train.AdamOptimizer(0.0002, beta1= 0.5).minimize(g_loss, var_list=g_vars, global_step=global_step)
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(0)
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction=0.2
